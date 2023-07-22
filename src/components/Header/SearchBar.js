a8011a7b0d87b674f7571d7c51cc83ea9130090f
Bishwash Adhikari <bishwashadhikari1@gmail.com>
2023-07-22 13:25:48 +0545

    Search bar has been modified to achieve the responsiveness (incomplete)

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 // The SearchBar component
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');  // State to store the search term
+  const [isActive, setIsActive] = useState(false); // State to store if search bar is active
+
+  const searchRef = useRef(null);
+
+  // Detect click outside searchbar to close it
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setIsActive(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  }, [searchRef]);
 
   return (
-    <div className="rounded-full border px-3 py-2 flex justify-between items-center">
+    <div className={`flex items-center rounded-full border px-3 py-2 sm:flex ${isActive ? 'flex' : 'hidden'}`} ref={searchRef}>
       <input 
         className="outline-none" 
         type="text" 
@@ -14,9 +31,12 @@ const SearchBar = () => {
         onChange={e => setSearchTerm(e.target.value)}  // Update state on input change
       />
       {searchTerm ? 
-        <i className="fas fa-times" onClick={() => setSearchTerm('')}></i> :  // If searchTerm is not empty, display cross icon
+        <i className="fas fa-times cursor-pointer" onClick={() => setSearchTerm('')}></i> :  // If searchTerm is not empty, display cross icon
         <i className="fas fa-search"></i>  // If searchTerm is empty, display search icon
       }
+      <div className={`sm:hidden ${isActive ? 'hidden' : 'block'} cursor-pointer`}>
+        <i className="fas fa-search" onClick={() => setIsActive(true)}></i>
+      </div>
     </div>
   );
 };
